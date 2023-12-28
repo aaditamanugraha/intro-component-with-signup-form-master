e5b8c5259d97e7904f457b1b3943029a583a7774
aaditamanugraha <adhityaaditaman@gmail.com>
2023-12-28 17:43:18 +0800

    Add form validity if first name is blank

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 const test = "test";
+
+const form = document.querySelector("form");
+
+const firstName = document.getElementById("first_name");
+const firstNameError = document.querySelector("p.error-text");
+
+firstName.addEventListener("input", (event) => {
+  if (firstName.validity.valid) {
+    firstNameError.textContent = "";
+  } else {
+    showError();
+  }
+});
+
+form.addEventListener("submit", (event) => {
+  if (!firstName.validity.valid) {
+    showError();
+    event.preventDefault();
+  }
+});
+
+function showError() {
+  if (firstName.validity.valueMissing) {
+    firstNameError.textContent = "First Name cannot be blank";
+  }
+}
